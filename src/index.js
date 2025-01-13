d8c430af158fcf81ad7d51a81ea3eecc28d18e47
adarsh311002 <adarsh.hit.aeie25@heritageit.edu.in>
2025-01-13 21:23:17 +0530

    added common middlewares and mongodB connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("hELLO FROM SERver !!");
+import { app } from "./app.js";
+import dotenv from "dotenv" ;
+import connectDB from "./db/index.js";
+
+
+
+dotenv.config({
+    path: "./.env"
+} 
+)
+
+const PORT = process.env.PORT || 8001;
+
+connectDB()
+.then(() => {
+    app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}.`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDb connection error",err)
+})
