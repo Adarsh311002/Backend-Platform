d6fd3b8c42e8ee43d069b024299af10940c3b2f4
adarsh311002 <adarsh.hit.aeie25@heritageit.edu.in>
2025-01-20 21:28:20 +0530

    routes for different controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,4 @@
-import { registerUser,logoutUser } from "../controllers/user.controllers.js";
+import { registerUser,logoutUser, loginUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, getUserChannelProfile, updateAccountDetails } from "../controllers/user.controllers.js";
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middlewares.js";
 import {jwtVerify} from "../middlewares/auth.middlewares.js";
@@ -7,6 +7,7 @@ import {jwtVerify} from "../middlewares/auth.middlewares.js";
 const router = Router();
 
 // Define the POST route for "/register"
+// unsecured routes
 router.route("/register").post(
     upload.fields([
         { name: "avatar", maxCount: 1 },
@@ -20,8 +21,19 @@ router.route("/register").post(
     registerUser
 );  
 
-// secure route
+router.route("/login").post(loginUser)
+router.route("/refresh-token").post(refreshAccessToken)
+
+// secured route
 
    router.route("/logout").post(jwtVerify,logoutUser);
+   router.route("/change-password").post(jwtVerify,changeCurrentPassword)
+   router.route("/current-user").get(jwtVerify,getCurrentUser)
+   //params use here
+   router.route("/c/:username").get(jwtVerify,getUserChannelProfile)
+
+   //
+   router.route("/update-account").patch(jwtVerify,updateAccountDetails)
+   
 
 export default router;
